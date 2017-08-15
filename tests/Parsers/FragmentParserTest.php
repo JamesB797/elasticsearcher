fd44742dc20f680352efbd7e3ad63fede488ee35
James Brown <james@upfeat.com>
2017-08-15 12:52:53 -0500

    Fix merging multiple fragments
    array_merge changes numeric keys, so if you don't unset before the merge, you
    risk unsetting the wrong key.

--- tests/Parsers/FragmentParserTest.php
+++ tests/Parsers/FragmentParserTest.php
@@ -7,133 +7,161 @@ use ElasticSearcher\Dummy\Fragments\Filters\IDFilter;
 
 class FragmentParserTest extends ElasticSearcherTestCase
 {
-	public function testParsingRootLevel()
-	{
-		$parser = new FragmentParser();
+    public function testParsingRootLevel()
+    {
+        $parser = new FragmentParser();
 
-		$body = [
-			'query' => new TermQuery('name', 'John'),
-		];
-		$expectedBody = [
-			'query' => [
-				'term' => [
-					'name' => 'John',
-				]
-			]
-		];
+        $body = [
+            'query' => new TermQuery('name', 'John'),
+        ];
+        $expectedBody = [
+            'query' => [
+                'term' => [
+                    'name' => 'John',
+                ]
+            ]
+        ];
 
-		$this->assertEquals($expectedBody, $parser->parse($body));
-	}
+        $this->assertEquals($expectedBody, $parser->parse($body));
+    }
 
-	public function testParsingChildLevel()
-	{
-		$parser = new FragmentParser();
+    public function testParsingChildLevel()
+    {
+        $parser = new FragmentParser();
 
-		$body = [
-			'query' => [
-				'bool' => [
-					'and' => [
-						new TermQuery('name', 'John'),
-						new TermQuery('category', 'authors'),
-					]
-				]
-			]
-		];
-		$expectedBody = [
-			'query' => [
-				'bool' => [
-					'and' => [
-						[
-							'term' => [
-								'name' => 'John'
-							]
-						],
-						[
-							'term' => [
-								'category' => 'authors'
-							]
-						],
-					]
-				]
-			]
-		];
+        $body = [
+            'query' => [
+                'bool' => [
+                    'and' => [
+                        new TermQuery('name', 'John'),
+                        new TermQuery('category', 'authors'),
+                    ]
+                ]
+            ]
+        ];
+        $expectedBody = [
+            'query' => [
+                'bool' => [
+                    'and' => [
+                        [
+                            'term' => [
+                                'name' => 'John'
+                            ]
+                        ],
+                        [
+                            'term' => [
+                                'category' => 'authors'
+                            ]
+                        ],
+                    ]
+                ]
+            ]
+        ];
 
-		$this->assertEquals($expectedBody, $parser->parse($body));
-	}
+        $this->assertEquals($expectedBody, $parser->parse($body));
+    }
 
-	public function testParsingNestedFragments()
-	{
-		$parser = new FragmentParser();
+    public function testParsingNestedFragments()
+    {
+        $parser = new FragmentParser();
 
-		$body = [
-			'query' => [
-				'bool' => [
-					'and' => [
-						new TermQuery('name', new TermQuery('category', 'authors')),
-					]
-				]
-			]
-		];
-		$expectedBody = [
-			'query' => [
-				'bool' => [
-					'and' => [
-						[
-							'term' => [
-								'name' => [
-									'term' => [
-										'category' => 'authors'
-									]
-								]
-							]
-						],
-					]
-				]
-			]
-		];
+        $body = [
+            'query' => [
+                'bool' => [
+                    'and' => [
+                        new TermQuery('name', new TermQuery('category', 'authors')),
+                    ]
+                ]
+            ]
+        ];
+        $expectedBody = [
+            'query' => [
+                'bool' => [
+                    'and' => [
+                        [
+                            'term' => [
+                                'name' => [
+                                    'term' => [
+                                        'category' => 'authors'
+                                    ]
+                                ]
+                            ]
+                        ],
+                    ]
+                ]
+            ]
+        ];
 
-		$this->assertEquals($expectedBody, $parser->parse($body));
-	}
+        $this->assertEquals($expectedBody, $parser->parse($body));
+    }
 
-	public function testParsingAndMergingWithParent()
-	{
-		$parser = new FragmentParser();
+    public function testParsingAndMergingWithParent()
+    {
+        $parser = new FragmentParser();
 
-		$body = [
-			'settings' => [
-				new StandardAnalyzer('myAnalyzer')
-			]
-		];
-		$expectedBody = [
-			'settings' => [
-				'myAnalyzer' => [
-					'type' => 'standard'
-				]
-			]
-		];
+        $body = [
+            'settings' => [
+                new StandardAnalyzer('myAnalyzer')
+            ]
+        ];
+        $expectedBody = [
+            'settings' => [
+                'myAnalyzer' => [
+                    'type' => 'standard'
+                ]
+            ]
+        ];
 
-		$this->assertEquals($expectedBody, $parser->parse($body));
-	}
+        $this->assertEquals($expectedBody, $parser->parse($body));
+    }
 
-	public function testParsingCustomFragments()
-	{
-		$parser = new FragmentParser();
+    public function testParsingAndMergingMultipleFragmentsWithParent()
+    {
+        $parser = new FragmentParser();
 
-		$body = [
-			'query' => [
-				new IDFilter(123),
-			]
-		];
-		$expectedBody = [
-			'query' => [
-				[
-					'term' => [
-						'id' => 123
-					]
-				]
-			]
-		];
+        $body = [
+            'settings' => [
+                new StandardAnalyzer('myAnalyzer1'),
+                new StandardAnalyzer('myAnalyzer2'),
+                new StandardAnalyzer('myAnalyzer3'),
+            ]
+        ];
+        $expectedBody = [
+            'settings' => [
+                'myAnalyzer1' => [
+                    'type' => 'standard'
+                ],
+                'myAnalyzer2' => [
+                    'type' => 'standard'
+                ],
+                'myAnalyzer3' => [
+                    'type' => 'standard'
+                ]
+            ]
+        ];
 
-		$this->assertEquals($expectedBody, $parser->parse($body));
-	}
+        $this->assertEquals($expectedBody, $parser->parse($body));
+    }
+
+    public function testParsingCustomFragments()
+    {
+        $parser = new FragmentParser();
+
+        $body = [
+            'query' => [
+                new IDFilter(123),
+            ]
+        ];
+        $expectedBody = [
+            'query' => [
+                [
+                    'term' => [
+                        'id' => 123
+                    ]
+                ]
+            ]
+        ];
+
+        $this->assertEquals($expectedBody, $parser->parse($body));
+    }
 }
